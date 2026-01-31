ed8328d9490cb1ada42d62241733dfe7035dee74
akashs <akashkumarsngh9@email.com>
2026-02-01 03:58:30 +0530

    - auto feature upadte

--- app/coaching/[coachingId]/section/[sectionId]/page.tsx
+++ app/coaching/[coachingId]/section/[sectionId]/page.tsx
@@ -1,5 +1,4 @@
 import SectionViewer from "@/components/section-viewer";
-import { coachingInstitutes } from "@/lib/mock-data";
 import { Suspense } from "react";
 
 interface PageProps {
@@ -9,28 +8,43 @@ interface PageProps {
     };
 }
 
+/* ======================================================
+   LOAD MASTER DATA (STATIC SAFE)
+====================================================== */
+
+async function loadCoachingData() {
+  const data = await import('@/lib/data.json')
+  return data.default ?? data
+}
 
 interface Props {
     params: Promise<{ coachingId: string; sectionId: string ,questionlist:any}>
 }
 export async function generateStaticParams() {
-    const params: Array<{ coachingId: string; sectionId: string }> = [];
+  const params: { coachingId: string; sectionId: string }[] = [];
 
-    coachingInstitutes.forEach((coaching) => {
-        Object.keys(coaching.sectionMap).forEach((id) => {
-            params.push({
-                coachingId: coaching.id,
-                sectionId: id,
-            });
-        });
-    });
+  const coachingInstitutes = await loadCoachingData();
 
-    return params;
+  for (const coaching of coachingInstitutes) {
+    if (!coaching.sectionMap) continue;
+
+    for (const sectionId of Object.keys(coaching.sectionMap)) {
+      params.push({
+        coachingId: String(coaching.id),
+        sectionId: String(sectionId),
+      });
+    }
+  }
+
+  return params;
 }
 
 
+
 export async function getQuestionsForSectionInCoaching(coachingId: string, sectionId: string) {
-    const coaching = coachingInstitutes.find((c) => c.id === coachingId);
+    const coachingInstitutes  = await loadCoachingData() as any;
+
+    const coaching = coachingInstitutes.find((c: { id: string; } ) => c.id === coachingId);
     if (!coaching) return [];
 
     const folder = coaching.folder_name;
