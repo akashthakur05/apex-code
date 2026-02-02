21c06a8af58321a084312eb766d436c5d237e5dc
akashs <akashkumarsngh9@email.com>
2026-02-03 00:48:03 +0530

    - speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import './globals.css'
 import Footer from '@/components/footer-home';
 import CoachingList from '@/components/coaching-list'
@@ -57,6 +58,7 @@ export default function RootLayout({
           <Footer />
         </div>
         <Analytics />
+        <SpeedInsights/>
       </body>
     </html>
   )
